7b9275df7d97f624563a9f32ef20a55bc821d5d8
Sergey Baboshin <ijustbsd@gmail.com>
2021-02-10 17:46:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from pysxf import SXF
 
 
 def main():
-    sxf = SXF('n-37-141.sxf')
-    print(sxf)
-
+    sxf = SXF('n-37-141.sxf', rsc_path='100t03g.rsc')
     sxf.parse()
+
+    for obj in sxf.objects:
+        rsc_obj = sxf.rsc.objects[obj.class_code]
+        prim = sxf.rsc.display_params[rsc_obj.internal_code]
 
 
 if __name__ == '__main__':
